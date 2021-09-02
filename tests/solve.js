e3a30827ae15c07a94c458ed549bf4f403d246a7
Mikhail <mumeu222@mail.ru>
2021-09-02 20:04:15 +0300

    refactor solve's tests

--- tests/solve.js
+++ tests/solve.js
@@ -20,126 +20,24 @@ QUnit.module('Тестируем функцию solve', function() {
         assert.strictEqual(solve('2+4*(5-x)', 1), 18);
     });
     QUnit.test('solve правильно обрабатывает ошибку с ошибками в выражении ', function(assert) {
-        assert.throws(function() {
-                solve('x + (x - 3))', 2);
-            },
-            function(err) {
-                return String(err.message) === EXPRESSION_ERROR;
-            },
-            'Error thrown');
-        assert.throws(function() {
-                solve('x + ((x - 1', 2);
-            },
-            function(err) {
-                return String(err.message) === EXPRESSION_ERROR;
-            },
-            'Error thrown');
-        assert.throws(function() {
-                solve('x + (x * (7 - ( 5 + 1))', 2);
-            },
-            function(err) {
-                return String(err.message) === EXPRESSION_ERROR;
-            },
-            'Error thrown');
-        assert.throws(function() {
-                solve('x + (7 - 1) * ((x - 1)', 2);
-            },
-            function(err) {
-                return String(err.message) === EXPRESSION_ERROR;
-            },
-            'Error thrown');
-        assert.throws(function() {
-                solve('()', 2);
-            },
-            function(err) {
-                return String(err.message) === EXPRESSION_ERROR;
-            },
-            'Error thrown');
-        assert.throws(function() {
-                solve('(+)', 2);
-            },
-            function(err) {
-                return String(err.message) === EXPRESSION_ERROR;
-            },
-            'Error thrown');
-        assert.throws(function() {
-                solve('x x x', 2);
-            },
-            function(err) {
-                return String(err.message) === EXPRESSION_ERROR;
-            },
-            'Error thrown');
-        assert.throws(function() {
-                solve('x++', 2);
-            },
-            function(err) {
-                return String(err.message) === EXPRESSION_ERROR;
-            },
-            'Error thrown');
-        assert.throws(function() {
-                solve('x ++ (5 - 1)', 2);
-            },
-            function(err) {
-                return String(err.message) === EXPRESSION_ERROR;
-            },
-            'Error thrown');
-        assert.throws(function() {
-                solve('x**2', 2);
-            },
-            function(err) {
-                return String(err.message) === EXPRESSION_ERROR;
-            },
-            'Error thrown');
+        assert.throws(() => solve('x + (x - 3))', 2), new Error(EXPRESSION_ERROR), 'Error thrown');
+        assert.throws(() => solve('x + ((x - 1', 2), new Error(EXPRESSION_ERROR), 'Error thrown');
+        assert.throws(() => solve('x + (x * (7 - ( 5 + 1))', 2), new Error(EXPRESSION_ERROR), 'Error thrown');
+        assert.throws(() => solve('x + (7 - 1) * ((x - 1)', 2), new Error(EXPRESSION_ERROR), 'Error thrown');
+        assert.throws(() => solve('()', 2), new Error(EXPRESSION_ERROR), 'Error thrown');
+        assert.throws(() => solve('(+)', 2), new Error(EXPRESSION_ERROR), 'Error thrown');
+        assert.throws(() => solve('x x x', 2), new Error(EXPRESSION_ERROR), 'Error thrown');
+        assert.throws(() => solve('x++', 2), new Error(EXPRESSION_ERROR), 'Error thrown');
+        assert.throws(() => solve('x ++ (5 - 1)', 2), new Error(EXPRESSION_ERROR), 'Error thrown');
+        assert.throws(() => solve('x**2', 2), new Error(EXPRESSION_ERROR), 'Error thrown');
     });
     QUnit.test('solve правильно обрабатывает ошибку с запрещенными символами в выражении ', function(assert) {
-        assert.throws(function() {
-                solve('x  + 4 -(  5 - 2)', 'a');
-            },
-            function(err) {
-                return String(err.message) === SYNTAX_ERROR;
-            },
-            'Error thrown');
-        assert.throws(function() {
-                solve('4 * 6 + (5 - 1 * x)', 'x');
-            },
-            function(err) {
-                return String(err.message) === SYNTAX_ERROR;
-            },
-            'Error thrown');
-        assert.throws(function() {
-                solve('2abc*x*3', 3);
-            },
-            function(err) {
-                return String(err.message) === SYNTAX_ERROR;
-            },
-            'Error thrown');
-        assert.throws(function() {
-                solve('x^100', 3);
-            },
-            function(err) {
-                return String(err.message) === SYNTAX_ERROR;
-            },
-            'Error thrown');
-        assert.throws(function() {
-                solve('a++', 3);
-            },
-            function(err) {
-                return String(err.message) === SYNTAX_ERROR;
-            },
-            'Error thrown');
-        assert.throws(function() {
-                solve('Hello + x', 3);
-            },
-            function(err) {
-                return String(err.message) === SYNTAX_ERROR;
-            },
-            'Error thrown');
-        assert.throws(function() {
-                solve(5, 5);
-            },
-            function(err) {
-                return String(err.message) === TYPE_ERROR;
-            },
-            'Error thrown');
+        assert.throws(() => solve('x  + 4 -(  5 - 2)', 'a'), new SyntaxError(SYNTAX_ERROR), 'Error thrown');
+        assert.throws(() => solve('4 * 6 + (5 - 1 * x)', 'x'), new SyntaxError(SYNTAX_ERROR), 'Error thrown');
+        assert.throws(() => solve('2abc*x*3', 3), new SyntaxError(SYNTAX_ERROR), 'Error thrown');
+        assert.throws(() => solve('x^100', 3), new SyntaxError(SYNTAX_ERROR), 'Error thrown');
+        assert.throws(() => solve('a++', 3), new SyntaxError(SYNTAX_ERROR), 'Error thrown');
+        assert.throws(() => solve('Hello + x', 3), new SyntaxError(SYNTAX_ERROR), 'Error thrown');
+        assert.throws(() => solve(5, 5), new TypeError(TYPE_ERROR), 'Error thrown');
     });
 });
